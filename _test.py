4c53a56009756f405599f5239943800e58e5e9f9
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2019-03-11 11:33:17 +0100

    major cleanup

--- _test.py
+++ _test.py
@@ -2,7 +2,7 @@ import pyxrootd
 import six
 import uproot
 
-print "Importing uproot and pyroott worked fine"
+print "Importing uproot and pyxroott worked fine"
 
 path = "/store/mc/RunIISummer16NanoAODv4/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/30000/3C337E12-9298-6D4C-961D-06F7CAD68921.root"
 path_prefix = "root://cmsxrootd.fnal.gov/"
@@ -25,13 +25,16 @@ branches = ["Electron_pt", "Muon_pt"]
 
 def uproot_iter(tree, branches, **kwargs):
     for start, stop, chunk in tree.iterate(branches, reportentries=True, **kwargs):
+        print "start", start
+        print "stop", stop
+        print "chunk", chunk
         for i in six.moves.range(stop - start):
             yield {key: arr[i] for key, arr in six.iteritems(chunk)}
 
 
 for i, event in enumerate(uproot_iter(tree, branches)):
-    print i, event
-    print event["Electron_pt"]
-    if(i > 20):
-        break
-        # pass
+    #print i, event
+    #print event["Electron_pt"]
+    # if(i > 3000):
+        # break
+    pass
